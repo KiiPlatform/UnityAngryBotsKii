cb5a59e350e4173ec8f59402a82f7eea79ec4a51
German Viscuso <germanviscuso@gmail.com>
2014-01-10 00:23:34 +0100

    Commented on how to get your own keys

--- Assets/Scripts/Backend/KiiInit.cs
+++ Assets/Scripts/Backend/KiiInit.cs
@@ -9,10 +9,12 @@ public class KiiInit : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		Debug.Log("KiiCloudInit");
+		// Create a Unity app at developer.kii.com and enter your own app_id, app_key and backend location here
 		Kii.Initialize("776e93b7", "b90ec1a893bd5abaa64ed1a25fbff4d0", Kii.Site.US);
 		
 		string deviceID = GetDeviceID();
 		Debug.Log("KiiAnalyticsInit - DeviceId: " + deviceID);
+		// Create a Unity app at developer.kii.com and enter your own app_id, app_key and backend location here
 		KiiAnalytics.Initialize("776e93b7", "b90ec1a893bd5abaa64ed1a25fbff4d0", KiiAnalytics.Site.US, deviceID);
 	}
 	
